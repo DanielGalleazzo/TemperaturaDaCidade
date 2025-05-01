dd3954ec46b139b5ea0a74ec0d0c998fa2f45663
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2025-05-01 00:47:04 -0300

    novas alterações

--- ProjetoTemperatura/Código/Program.cs
+++ ProjetoTemperatura/Código/Program.cs
@@ -9,27 +9,35 @@ class Program
     {
         Console.Write("Digite a cidade: ");
         string cidade = Console.ReadLine();
-        string apiKey = "Insira aqui a sua api"; 
+        string apiKey = "3ffe0230a7417daf36c0c422d9206e39";
         string url = $"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={apiKey}&units=metric&lang=pt";
 
-        using (HttpClient client = new HttpClient())
+        using (HttpClient cliente = new HttpClient())
         {
             try
             {
-                HttpResponseMessage response = await client.GetAsync(url);
+                HttpResponseMessage response = await cliente.GetAsync(url);
                 response.EnsureSuccessStatusCode();
-
                 string resposta = await response.Content.ReadAsStringAsync();
                 JObject clima = JObject.Parse(resposta);
-
                 string descricao = clima["weather"][0]["description"].ToString();
-                string temperatura = clima["main"]["temp"].ToString();
+                double temperatura = Convert.ToDouble(clima["main"]["temp"].ToString());
+                Console.WriteLine($"Clima em:" + cidade + " Condição do clima:" + descricao + " Sua temperatura é de: "+ temperatura + "°C");
+                if (temperatura < 15)
+                {
+                    Console.WriteLine("Leve uma blusa, está frio");
+                }
+                if (descricao.Contains("Chuva", StringComparison.OrdinalIgnoreCase) ||
+                    descricao.Contains("Nublado", StringComparison.OrdinalIgnoreCase) ||
+                    descricao.Contains("Chovendo", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Usar guarda-chuva, pode estar chovendo ou choverá");
+                }
 
-                Console.WriteLine($"Clima em: "+ cidade + descricao + temperatura + "°C");
             }
-            catch (HttpRequestException e)
+            catch (Exception error)
             {
-                Console.WriteLine("Erro ao obter os dados: " + e.Message);
+                Console.WriteLine("Código do erro: " + error.Message);
             }
         }
     }
--- ProjetoTemperatura/obj/Debug/net8.0/ProjetoTemperatura.AssemblyInfo.cs
+++ ProjetoTemperatura/obj/Debug/net8.0/ProjetoTemperatura.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoTemperatura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1386393d8f67951cddeae1cdff2307d8f6c10a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoTemperatura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoTemperatura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
