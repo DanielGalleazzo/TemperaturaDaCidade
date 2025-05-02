33daccdc7cf2be715d0712d27ea40df77feb6f47
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2025-05-02 13:09:29 -0300

    Converção de celsius para fahrenheit

--- ProjetoTemperatura/Código/Program.cs
+++ ProjetoTemperatura/Código/Program.cs
@@ -9,36 +9,83 @@ class Program
     {
         Console.Write("Digite a cidade: ");
         string cidade = Console.ReadLine();
+        Console.WriteLine("Você quer em celsius ou fahrenheit ?");
+        string TipoTemperatura = Console.ReadLine();
         string apiKey = "3ffe0230a7417daf36c0c422d9206e39";
         string url = $"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={apiKey}&units=metric&lang=pt";
 
-        using (HttpClient cliente = new HttpClient())
+        if (TipoTemperatura.Contains("Celsius", StringComparison.OrdinalIgnoreCase))
         {
-            try
-            {
-                HttpResponseMessage response = await cliente.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                string resposta = await response.Content.ReadAsStringAsync();
-                JObject clima = JObject.Parse(resposta);
-                string descricao = clima["weather"][0]["description"].ToString();
-                double temperatura = Convert.ToDouble(clima["main"]["temp"].ToString());
-                Console.WriteLine($"Clima em:" + cidade + " Condição do clima:" + descricao + " Sua temperatura é de: "+ temperatura + "°C");
-                if (temperatura < 15)
-                {
-                    Console.WriteLine("Leve uma blusa, está frio");
-                }
-                if (descricao.Contains("Chuva", StringComparison.OrdinalIgnoreCase) ||
-                    descricao.Contains("Nublado", StringComparison.OrdinalIgnoreCase) ||
-                    descricao.Contains("Chovendo", StringComparison.OrdinalIgnoreCase))
-                {
-                    Console.WriteLine("Usar guarda-chuva, pode estar chovendo ou choverá");
-                }
 
-            }
-            catch (Exception error)
+            using (HttpClient cliente = new HttpClient())
             {
-                Console.WriteLine("Código do erro: " + error.Message);
+                try
+                {
+                    HttpResponseMessage response = await cliente.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string resposta = await response.Content.ReadAsStringAsync();
+                    JObject clima = JObject.Parse(resposta);
+                    string descricao = clima["weather"][0]["description"].ToString();
+                    double temperatura = Convert.ToDouble(clima["main"]["temp"].ToString());
+                    Console.WriteLine($"Clima em:" + cidade + " Condição do clima:" + descricao + " Sua temperatura é de: " + temperatura + "°C");
+                    if (temperatura < 15)
+                    {
+                        Console.WriteLine("Leve uma blusa, está frio");
+                    }
+                    if (descricao.Contains("Chuva", StringComparison.OrdinalIgnoreCase) ||
+                        descricao.Contains("Nublado", StringComparison.OrdinalIgnoreCase) ||
+                        descricao.Contains("Chovendo", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Usar guarda-chuva, pode estar chovendo ou choverá");
+                    }
+
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Código do erro: " + error.Message);
+                }
+            }
+
+        }
+        if (TipoTemperatura.Contains("Fahrenheit", StringComparison.OrdinalIgnoreCase))
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await cliente.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string resposta = await response.Content.ReadAsStringAsync();
+                    JObject clima = JObject.Parse(resposta);
+                    string descricao = clima["weather"][0]["description"].ToString();
+                    double temperatura = Convert.ToDouble(clima["main"]["temp"].ToString());
+                    double temperaturaFah = (temperatura * 1.8) + 32;
+                    Console.WriteLine($"Clima em:" + cidade + " Condição do clima:" + descricao + " Sua temperatura é de: " + temperaturaFah + "°F");
+                    if (temperaturaFah < 59)
+                    {
+                        Console.WriteLine("Leve uma blusa, está frio");
+                    }
+                    if (descricao.Contains("Chuva", StringComparison.OrdinalIgnoreCase) ||
+                        descricao.Contains("Nublado", StringComparison.OrdinalIgnoreCase) ||
+                        descricao.Contains("Chovendo", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Usar guarda-chuva, pode estar chovendo ou choverá");
+                    }
+                    if (temperaturaFah <= 77)
+                    {
+                        Console.WriteLine("Relativamente calor");
+                    }
+
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Código do erro: " + error.Message);
+                }
             }
         }
     }
 }
+
+
